ae9b95072d65d3199979334bc4cf908f6e109fa7
WilliamKelly00 <williammkelly00@gmail.com>
2022-03-08 00:09:55 -0500

    added profile information

---
+++ pages/profile.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Logo from '../components/Logo'
+
+export default function profile() {
+    let x = 12512512;
+    let y = 25125123;
+  return (
+    <div>
+        <Logo/>
+        <p>profile pic</p>
+        <p>{x} Points = {y} TorE Tokens</p>
+        <p>{y} TorE Tokens = $702,215 USD</p>
+        <p>Buy TorE</p> <Logo/> <p>Sell TorE</p>
+        <p>$494M Market Cap</p> <p>$18.6M Daily Volume</p> <p>Ranked #198!</p>
+        <p>TorE Exchange</p>
+
+
+    </div>
+  )
+}
