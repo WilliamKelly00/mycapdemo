ee1f9120587b269b2a10387d85e6d0cab33df863
WilliamKelly00 <williammkelly00@gmail.com>
2022-03-08 00:23:56 -0500

    topic scaffoldnig

--- pages/topics.jsx
+++ pages/topics.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import Logo from '../components/Logo';
 
 export default function topics() {
+    const topics = ["Biology", "Biology","Biology","Biology","Biology","Biology","Biology","Biology","Biology",];
+
   return (
-    <div>topics</div>
+    <div>
+        <Logo/>
+        <p>Educational Queries</p>
+        <button>Filter</button>
+
+        {topics.map(topic => (
+            <div>{topic}</div>
+        ))}
+
+    </div>
   )
 }
