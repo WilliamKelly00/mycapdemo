dd09d0e3edf751adcb88228ad85bd70c90e78cc7
WilliamKelly00 <williammkelly00@gmail.com>
2022-03-09 01:56:43 -0500

    profile

--- pages/profile.jsx
+++ pages/profile.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Logo from '../components/Logo'
+import styles from '../styles/Profile.module.css'
 
 export default function profile() {
     let x = 12512512;
@@ -7,12 +8,17 @@ export default function profile() {
   return (
     <div>
         <Logo/>
-        <p>profile pic</p>
-        <p>{x} Points = {y} TorE Tokens</p>
-        <p>{y} TorE Tokens = $702,215 USD</p>
-        <p>Buy TorE</p> <Logo/> <p>Sell TorE</p>
-        <p>$494M Market Cap</p> <p>$18.6M Daily Volume</p> <p>Ranked #198!</p>
-        <p>TorE Exchange</p>
+        <div className={styles.pic}>Profile pic</div>
+        
+        <div className={styles.profileholder}>
+          <p>{x} Points = {y} TorE Tokens</p>
+          <p>{y} TorE Tokens = $702,215 USD</p>
+          <p>Buy TorE</p>
+          <p>Sell TorE</p>
+          <p>Truth or Err</p>
+          <p>$494M Market Cap</p> <p>$18.6M Daily Volume</p> <p>Ranked #198!</p>
+          <p>TorE Exchange</p>
+        </div>
 
 
     </div>
