711b0c3904b7b8f4e3b309cf7a6c397e56d54312
WilliamKelly00 <williammkelly00@gmail.com>
2022-03-09 01:34:21 -0500

    sign in + sign up styles

--- pages/signin.jsx
+++ pages/signin.jsx
@@ -1,24 +1,27 @@
 import React from 'react'
 import Logo from '../components/Logo'
+import styles from '../styles/Sign.module.css'
 
 export default function signin() {
 
   return (
     <div>
       <Logo/>
-        <form>
-        <label>
+        <form className={styles.formstyle}>
+        <label className={styles.inputbox}>
             Username:
             <input type="text" name="username" />
         </label>
-        <label>
+        <label className={styles.inputbox}>
             Password:
             <input type="text" name="password" />
         </label>
-        <button type="submit">Log in</button>
+        <button className={styles.login} type="submit">Log in</button>
         </form>
-        <button>Sign in with Google</button>
-        <button>Sign in with Facebook</button>
+        <div className={styles.thirdparty}>
+          <button className={styles.google}>Sign in with Google</button>
+          <button className={styles.fb}>Sign in with Facebook</button>
+        </div>
     </div>
   )
 }
